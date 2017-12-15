97bb17d474a1ff5231f1de3198ad616d3e4a9ad6
Tu Ngo <tungo8810@gmail.com>
2017-12-14 17:01:28 -0800

    Add Api controller and routes

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,9 @@
+class ApiController < ApplicationController
+  def index
+    render plain: 'The Email API'
+  end
+
+  def send_email
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'api#index'
+  post '/send', format: :json, to: 'api#send_email'
 end
