c9e89204d821793bbac2b671154de21eb36073c1
Tu Ngo <tungo8810@gmail.com>
2017-12-14 17:19:33 -0800

    Add config for gmail smtp

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,21 +4,24 @@ class ApiController < ApplicationController
   end
 
   def send_email
+    # check "to" param
     unless params[:to]
       render json: { error: 'missing parameter' }, status: 422
       return
     end
 
+    # handle send email error
     begin
       ApiMailer.send_email(api_params).deliver_now
     rescue
-      render json: { error: 'an error occurred when sending your email' }, status: 422
+      render json: { error: 'an error occurred when sending your email' }, status: 500
       return
     end
 
     render json: { success: 'your email has been sent' }
   end
 
+  # params helper
   def api_params
     params.permit :to, :subject, :body
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # use gmail smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default charset: "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "",
+    password: ""
+  }
 end
