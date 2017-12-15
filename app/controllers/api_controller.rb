ccda53a593ca5dd2ae393e71b119d9ef365e2c6c
Tu Ngo <tungo8810@gmail.com>
2017-12-14 17:15:07 -0800

    Add method ApiController#send_email

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,6 +4,22 @@ class ApiController < ApplicationController
   end
 
   def send_email
+    unless params[:to]
+      render json: { error: 'missing parameter' }, status: 422
+      return
+    end
 
+    begin
+      ApiMailer.send_email(api_params).deliver_now
+    rescue
+      render json: { error: 'an error occurred when sending your email' }, status: 422
+      return
+    end
+
+    render json: { success: 'your email has been sent' }
+  end
+
+  def api_params
+    params.permit :to, :subject, :body
   end
 end
